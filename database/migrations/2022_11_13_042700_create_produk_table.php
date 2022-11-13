d06d0575bb7b49f49237068ba175ac4f3a14d2fd
karyakirana <adibpratama2@gmail.com>
2022-11-13 11:41:24 +0700

    first commit

---
+++ database/migrations/2022_11_13_042700_create_produk_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('barang', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('produk_kategori_id');
+            $table->string('nama_produk');
+            $table->text('tipe');
+            $table->string('isi_kemasan');
+            $table->string('satuan_beli');
+            $table->string('satuan_jual');
+            $table->bigInteger('harga');
+            $table->text('keterangan');
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('barang');
+    }
+};
