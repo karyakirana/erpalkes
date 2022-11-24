14a9d9ba36e319fc3661c683c3eaab109fa83e70
karyakirana <adibpratama2@gmail.com>
2022-11-24 07:46:17 +0700

    Sales Area Fix

--- app/Models/Master/SalesArea.php
+++ app/Models/Master/SalesArea.php
@@ -2,16 +2,23 @@
 
 namespace App\Models\Master;
 
+use App\Models\KodeTrait;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class SalesArea extends Model
 {
     use HasFactory;
+
     protected $table = 'sales_area';
     protected $fillable = [
         'kode_area',
         'nama_area',
         'keterangan',
     ];
+
+    public function getLastNumMasterAttribute()
+    {
+        return substr($this->kode_area, 1, 5);
+    }
 }
