42139196caaeba49df7d3c7371c7700efa64dad6
karyakirana <adibpratama2@gmail.com>
2022-11-24 18:27:36 +0700

    produk mock up

---
+++ app/Http/Controllers/Master/ProdukController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers\Master;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class ProdukController extends Controller
+{
+    public function index()
+    {
+        //
+    }
+
+    public function datatables(Request $request)
+    {
+        //
+    }
+
+    public function destroy(Request $request)
+    {
+        //
+    }
+}
