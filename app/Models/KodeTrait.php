ad58a189e2c1a839b9d6bebb464e08198331eee4
karyakirana <adibpratama2@gmail.com>
2022-11-24 18:36:08 +0700

    modal

---
+++ app/Models/KodeTrait.php
@@ -0,0 +1,19 @@
+<?php namespace App\Models;
+
+trait KodeTrait
+{
+    public function getLastNumMasterAttribute()
+    {
+        return substr($this->kode, 1, 5);
+    }
+
+    public function getLastNumTransAttribute()
+    {
+        return substr($this->kode, 0, 4);
+    }
+
+    public function getLastNumCharAttribute()
+    {
+        return substr($this->kode, 0, strpos($this->kode, '/'));
+    }
+}
