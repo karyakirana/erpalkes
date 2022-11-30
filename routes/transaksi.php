aa395da41ace8980b59856ce8410f41a012af5b7
toby <tobytriantono@gmail.com>
2022-11-30 22:30:56 +0700

    routing transaksi toby

--- routes/transaksi.php
+++ routes/transaksi.php
@@ -1,3 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+
+Route::get('penjualan', [\App\Http\Controllers\Penjualan\PenjualanController::class, 'index'])->name('penjualan');
+Route::get('penjualan/form', \App\Http\Livewire\Penjualan\PenjualanForm::class)->name('penjualan.form');
+
+Route::get('penjualan/retur', [\App\Http\Controllers\Penjualan\PenjualanReturController::class, 'index'])->name('penjualan.retur');
+Route::get('penjualan/retur/form', \App\Http\Livewire\Penjualan\PenjualanReturForm::class)->name('penjualan.retur.form');
