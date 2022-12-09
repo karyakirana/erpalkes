74f36d86deda8d24ca74668aafbdbfe2b12383d2
toby <tobytriantono@gmail.com>
2022-12-09 20:24:41 +0700

    update detail produk

---
+++ resources/views/pages/master/produk-show.blade.php
@@ -0,0 +1,197 @@
+<x-metronics-layout>
+    <!--begin::Main-->
+    <div class="app-main flex-column flex-row-fluid" id="kt_app_main">
+        <!--begin::Content wrapper-->
+        <div class="d-flex flex-column flex-column-fluid">
+            <!--begin::Toolbar-->
+            <div id="kt_app_toolbar" class="app-toolbar py-3 py-lg-6">
+                <!--begin::Toolbar container-->
+                <div id="kt_app_toolbar_container" class="app-container container-xxl d-flex flex-stack">
+                    <!--begin::Page title-->
+                    <div class="page-title d-flex flex-column justify-content-center flex-wrap me-3">
+                        <!--begin::Title-->
+                        <h1 class="page-heading d-flex text-dark fw-bold fs-3 flex-column justify-content-center my-0">Edit Product</h1>
+                        <!--end::Title-->
+                        <!--begin::Breadcrumb-->
+                        <ul class="breadcrumb breadcrumb-separatorless fw-semibold fs-7 my-0 pt-1">
+                            <!--begin::Item-->
+                            <li class="breadcrumb-item text-muted">
+                                <a href="/metronic8/demo1/../demo1/index.html" class="text-muted text-hover-primary">Master</a>
+                            </li>
+                            <!--end::Item-->
+                            <!--begin::Item-->
+                            <li class="breadcrumb-item">
+                                <span class="bullet bg-gray-400 w-5px h-2px"></span>
+                            </li>
+                            <!--end::Item-->
+                            <!--begin::Item-->
+                            <li class="breadcrumb-item text-muted">Product</li>
+                            <!--end::Item-->
+                            <!--begin::Item-->
+                            <li class="breadcrumb-item">
+                                <span class="bullet bg-gray-400 w-5px h-2px"></span>
+                            </li>
+                            <!--end::Item-->
+                            <!--begin::Item-->
+                            <li class="breadcrumb-item text-muted">Detail</li>
+                            <!--end::Item-->
+                        </ul>
+                        <!--end::Breadcrumb-->
+                    </div>
+                    <!--end::Page title-->
+
+                </div>
+                <!--end::Toolbar container-->
+            </div>
+            <!--end::Toolbar-->
+            <!--begin::Content-->
+            <div id="kt_app_content" class="app-content flex-column-fluid">
+                <!--begin::Content container-->
+                <div id="kt_app_content_container" class="app-container container-xxl">
+                    <!--begin::Form-->
+                    <form id="kt_ecommerce_add_product_form" class="form d-flex flex-column flex-lg-row" data-kt-redirect="/metronic8/demo1/../demo1/apps/ecommerce/catalog/products.html">
+                        <!--begin::Aside column-->
+                        <div class="d-flex flex-column gap-7 gap-lg-10 w-100 w-lg-300px mb-7 me-lg-10">
+
+                            <!--begin::Thumbnail settings-->
+                            <livewire:master.produk-gambar-detail/>
+
+                            <!--begin::Status-->
+                            <div class="card card-flush py-4">
+                                <!--begin::Card header-->
+                                <div class="card-header">
+                                    <!--begin::Card title-->
+                                    <div class="card-title">
+                                        <h2>Status</h2>
+                                    </div>
+                                    <!--end::Card title-->
+                                    <!--begin::Card toolbar-->
+                                    <div class="card-toolbar">
+                                        <div class="rounded-circle bg-success w-15px h-15px" id="kt_ecommerce_add_product_status"></div>
+                                    </div>
+                                    <!--begin::Card toolbar-->
+                                </div>
+                                <!--end::Card header-->
+                                <!--begin::Card body-->
+                                <div class="card-body pt-0">
+                                    <!--begin::Select2-->
+                                    <select class="form-select mb-2" data-control="select2" data-hide-search="true" data-placeholder="Select an option" id="kt_ecommerce_add_product_status_select">
+                                        <option></option>
+                                        <option value="published" selected="selected">Published</option>
+                                        <option value="draft">Draft</option>
+                                        <option value="scheduled">Scheduled</option>
+                                        <option value="inactive">Inactive</option>
+                                    </select>
+                                    <!--end::Select2-->
+                                    <!--begin::Description-->
+                                    <div class="text-muted fs-7">Set the product status.</div>
+                                    <!--end::Description-->
+                                    <!--begin::Datepicker-->
+                                    <div class="d-none mt-10">
+                                        <label for="kt_ecommerce_add_product_status_datepicker" class="form-label">Select publishing date and time</label>
+                                        <input class="form-control" id="kt_ecommerce_add_product_status_datepicker" placeholder="Pick date & time" />
+                                    </div>
+                                    <!--end::Datepicker-->
+                                </div>
+                                <!--end::Card body-->
+                            </div>
+                            <!--end::Status-->
+
+                            <!--begin::Category & tags-->
+                            <livewire:master.produk-kategori-detail/>
+
+
+                            <!--begin::Card widget 6-->
+                            <div class="card card-flush">
+                                <!--begin::Header-->
+                                <div class="card-header pt-5">
+                                    <!--begin::Title-->
+                                    <div class="card-title d-flex flex-column">
+                                        <!--begin::Info-->
+                                        <div class="d-flex align-items-center">
+                                            <!--begin::Currency-->
+                                            <span class="fs-4 fw-semibold text-gray-400 me-1 align-self-start">$</span>
+                                            <!--end::Currency-->
+                                            <!--begin::Amount-->
+                                            <span class="fs-2hx fw-bold text-dark me-2 lh-1 ls-n2">2,420</span>
+                                            <!--end::Amount-->
+                                            <!--begin::Badge-->
+                                            <span class="badge badge-light-success fs-base">
+															<!--begin::Svg Icon | path: icons/duotune/arrows/arr066.svg-->
+															<span class="svg-icon svg-icon-5 svg-icon-success ms-n1">
+																<svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+																	<rect opacity="0.5" x="13" y="6" width="13" height="2" rx="1" transform="rotate(90 13 6)" fill="currentColor" />
+																	<path d="M12.5657 8.56569L16.75 12.75C17.1642 13.1642 17.8358 13.1642 18.25 12.75C18.6642 12.3358 18.6642 11.6642 18.25 11.25L12.7071 5.70711C12.3166 5.31658 11.6834 5.31658 11.2929 5.70711L5.75 11.25C5.33579 11.6642 5.33579 12.3358 5.75 12.75C6.16421 13.1642 6.83579 13.1642 7.25 12.75L11.4343 8.56569C11.7467 8.25327 12.2533 8.25327 12.5657 8.56569Z" fill="currentColor" />
+																</svg>
+															</span>
+                                                <!--end::Svg Icon-->2.6%</span>
+                                            <!--end::Badge-->
+                                        </div>
+                                        <!--end::Info-->
+                                        <!--begin::Subtitle-->
+                                        <span class="text-gray-400 pt-1 fw-semibold fs-6">Average Daily Sales</span>
+                                        <!--end::Subtitle-->
+                                    </div>
+                                    <!--end::Title-->
+                                </div>
+                                <!--end::Header-->
+                                <!--begin::Card body-->
+                                <div class="card-body d-flex align-items-end px-0 pb-0">
+                                    <!--begin::Chart-->
+                                    <div id="kt_card_widget_6_chart" class="w-100" style="height: 80px"></div>
+                                    <!--end::Chart-->
+                                </div>
+                                <!--end::Card body-->
+                            </div>
+                            <!--end::Card widget 6-->
+                            <!--begin::Template settings-->
+                            <div class="card card-flush py-4">
+                                <!--begin::Card header-->
+                                <div class="card-header">
+                                    <!--begin::Card title-->
+                                    <div class="card-title">
+                                        <h2>Product Template</h2>
+                                    </div>
+                                    <!--end::Card title-->
+                                </div>
+                                <!--end::Card header-->
+                                <!--begin::Card body-->
+                                <div class="card-body pt-0">
+                                    <!--begin::Select store template-->
+                                    <label for="kt_ecommerce_add_product_store_template" class="form-label">Select a product template</label>
+                                    <!--end::Select store template-->
+                                    <!--begin::Select2-->
+                                    <select class="form-select mb-2" data-control="select2" data-hide-search="true" data-placeholder="Select an option" id="kt_ecommerce_add_product_store_template">
+                                        <option></option>
+                                        <option value="default" selected="selected">Default template</option>
+                                        <option value="electronics">Electronics</option>
+                                        <option value="office">Office stationary</option>
+                                        <option value="fashion">Fashion</option>
+                                    </select>
+                                    <!--end::Select2-->
+                                    <!--begin::Description-->
+                                    <div class="text-muted fs-7">Assign a template from your current theme to define how a single product is displayed.</div>
+                                    <!--end::Description-->
+                                </div>
+                                <!--end::Card body-->
+                            </div>
+                            <!--end::Template settings-->
+                        </div>
+                        <!--end::Aside column-->
+
+
+                        <!--begin::Main column-->
+                        <livewire:master.produk-form-detail/>
+
+                    </form>
+                    <!--end::Form-->
+                </div>
+                <!--end::Content container-->
+            </div>
+            <!--end::Content-->
+        </div>
+        <!--end::Content wrapper-->
+
+    </div>
+    <!--end:::Main-->
+</x-metronics-layout>
