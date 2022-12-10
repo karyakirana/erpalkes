5250f003818406fbec79aa6eb482c88825798cf0
toby <tobytriantono@gmail.com>
2022-12-10 13:01:18 +0700

    update detail produk

--- app/Http/Livewire/Master/ProdukFormDetail.php
+++ app/Http/Livewire/Master/ProdukFormDetail.php
@@ -9,25 +9,25 @@ use Livewire\Component;
 class ProdukFormDetail extends Component
 {
 
-    use ProdukFormTrait;
-
-    public function mount($produk_id)
-    {
-        $produk = ProdukRepository::getById($produk_id);
-        $this->loadProduk($produk_id);
-    }
-
-    public function edit()
-    {
-        $this->emit('');
-    }
-
-    public function update()
-    {
-        $data = $this->validate();
-        $repo = ProdukRepository::update($data, $this->produk_id);
-        $this->emit();
-    }
+//    use ProdukFormTrait;
+//
+//    public function mount($produk_id)
+//    {
+//        $produk = ProdukRepository::getById($produk_id);
+//        $this->loadProduk($produk_id);
+//    }
+//
+//    public function edit()
+//    {
+//        $this->emit('');
+//    }
+//
+//    public function update()
+//    {
+//        $data = $this->validate();
+//        $repo = ProdukRepository::update($data, $this->produk_id);
+//        $this->emit();
+//    }
 
 
     public function render()
