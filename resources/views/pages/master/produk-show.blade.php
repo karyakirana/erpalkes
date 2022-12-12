542e7b9b51ec11ad1f31395d0ade782102b70166
kirana <adibpratama2@gmail.com>
2022-12-12 18:23:59 +0700

    update

--- resources/views/pages/master/produk-show.blade.php
+++ resources/views/pages/master/produk-show.blade.php
@@ -1,5 +1,5 @@
 <x-metronics-layout>
-    !--begin::Main-->
+    <!--begin::Main-->
     <div class="app-main flex-column flex-row-fluid" id="kt_app_main">
         <!--begin::Content wrapper-->
         <div class="d-flex flex-column flex-column-fluid">
