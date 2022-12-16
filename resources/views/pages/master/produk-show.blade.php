918bc88a49364183055f1e9f37a80ee2affcb92f
toby <tobytriantono@gmail.com>
2022-12-17 01:57:39 +0700

    update detail produk

--- resources/views/pages/master/produk-show.blade.php
+++ resources/views/pages/master/produk-show.blade.php
@@ -22,6 +22,7 @@
                             <!--begin::Item-->
                             <li class="breadcrumb-item">
                                 <span class="bullet bg-gray-400 w-5px h-2px"></span>
+
                             </li>
                             <!--end::Item-->
                             <!--begin::Item-->
