476750abe23db9f73b3741df4bb79a9b6d597106
kirana <adibpratama2@gmail.com>
2022-12-20 18:07:32 +0700

    component

--- resources/views/components/input/text.blade.php
+++ resources/views/components/input/text.blade.php
@@ -1,3 +1,4 @@
 @aware(['error', 'name'])
 @props(['error'=>null, 'name'=>null])
 <input type="text" name="{{$name}}" {{ $attributes->class(['form-control', 'is-invalid'=>$errors->has($name)])->merge() }}>
+<x-input.error :messages="$errors->get($name)" />
