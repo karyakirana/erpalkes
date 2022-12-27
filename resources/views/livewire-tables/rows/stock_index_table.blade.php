1e889a8abb0825ff7ebe7b8912cd35115e763226
karyakirana <adibpratama2@gmail.com>
2022-12-28 02:20:51 +0700

    stock table index

--- resources/views/livewire-tables/rows/stock_index_table.blade.php
+++ resources/views/livewire-tables/rows/stock_index_table.blade.php
@@ -1,19 +1,19 @@
-<x-livewire-tables::bs.5.table.cell width="10%">
+<x-livewire-tables::bs5.table.cell width="10%">
     {{$row->produk->kode}}
-</x-livewire-tables::bs.5.table.cell>
-<x-livewire-tables::bs.5.table.cell width="35%">
+</x-livewire-tables::bs5.table.cell>
+<x-livewire-tables::bs5.table.cell width="35%">
     {{$row->produk->nama}} <br>
     {{$row->tgl_expired}}
-</x-livewire-tables::bs.5.table.cell>
-<x-livewire-tables::bs.5.table.cell width="15%">
+</x-livewire-tables::bs5.table.cell>
+<x-livewire-tables::bs5.table.cell width="15%">
     {{$row->stock_awal}}
-</x-livewire-tables::bs.5.table.cell>
-<x-livewire-tables::bs.5.table.cell width="15%">
+</x-livewire-tables::bs5.table.cell>
+<x-livewire-tables::bs5.table.cell width="15%">
     {{$row->stock_masuk}}
-</x-livewire-tables::bs.5.table.cell>
-<x-livewire-tables::bs.5.table.cell width="15%">
+</x-livewire-tables::bs5.table.cell>
+<x-livewire-tables::bs5.table.cell width="15%">
     {{$row->stock_keluar}}
-</x-livewire-tables::bs.5.table.cell>
-<x-livewire-tables::bs.5.table.cell width="15%">
+</x-livewire-tables::bs5.table.cell>
+<x-livewire-tables::bs5.table.cell width="15%">
     {{$row->stock_saldo}}
-</x-livewire-tables::bs.5.table.cell>
+</x-livewire-tables::bs5.table.cell>
