da0a22a23c6f6f088aaf85f0ba896a8faefa33fa
toby <tobytriantono@gmail.com>
2022-12-27 13:49:08 +0700

    update view datatable

---
+++ app/Http/Livewire/Datatables/JabatanIndexTable.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Livewire\Datatables;
+
+use Illuminate\Database\Eloquent\Builder;
+use Rappasoft\LaravelLivewireTables\DataTableComponent;
+use Rappasoft\LaravelLivewireTables\Views\Column;
+
+class JabatanIndexTable extends DataTableComponent
+{
+
+    public function columns(): array
+    {
+        return [
+            Column::make('Column Name'),
+        ];
+    }
+
+    public function query(): Builder
+    {
+
+    }
+}
