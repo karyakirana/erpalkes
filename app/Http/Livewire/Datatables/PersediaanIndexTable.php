e7ccd34edc2a82f62516c9b8782dad2fb6e28ddf
karyakirana <adibpratama2@gmail.com>
2022-12-28 02:28:11 +0700

    persediaan table index

--- app/Http/Livewire/Datatables/PersediaanIndexTable.php
+++ app/Http/Livewire/Datatables/PersediaanIndexTable.php
@@ -2,23 +2,30 @@
 
 namespace App\Http\Livewire\Datatables;
 
+use App\Models\Persediaan\Persediaan;
 use Illuminate\Database\Eloquent\Builder;
 use Rappasoft\LaravelLivewireTables\DataTableComponent;
 use Rappasoft\LaravelLivewireTables\Views\Column;
 
 class PersediaanIndexTable extends DataTableComponent
 {
+    use LivewireDatatableTrait;
 
     public function columns(): array
     {
         return [
-            Column::make('Column Name'),
+            Column::make('Produk', 'produk.nama'),
+            Column::make('Harga', 'harga'),
+            Column::make('Stock Awal', 'stock_awal'),
+            Column::make('Stock Masuk', 'stock_masuk'),
+            Column::make('Stock Keluar', 'stock_keluar'),
+            Column::make('Stock Saldo', 'stock_saldo'),
         ];
     }
 
     public function query(): Builder
     {
-
+        return Persediaan::query();
     }
 
     public function rowView(): string
--- resources/views/livewire-tables/rows/persediaan_index_table.blade.php
+++ resources/views/livewire-tables/rows/persediaan_index_table.blade.php
@@ -1,5 +1,19 @@
-<x-livewire-tables::table.cell>
-{{-- Note: This is a tailwind cell --}}
-{{-- For bootstrap 4, use <x-livewire-tables::bs4.table.cell> --}}
-{{-- For bootstrap 5, use <x-livewire-tables::bs5.table.cell> --}}
-</x-livewire-tables::table.cell>
+<x-livewire-tables::bs5.table.cell width="30%">
+    {{$row->produk->nama}} <br>
+    {{$row->tgl_expired}}
+</x-livewire-tables::bs5.table.cell>
+<x-livewire-tables::bs5.table.cell width="15%">
+    {{$row->harga}}
+</x-livewire-tables::bs5.table.cell>
+<x-livewire-tables::bs5.table.cell width="15%">
+    {{$row->stock_awal}}
+</x-livewire-tables::bs5.table.cell>
+<x-livewire-tables::bs5.table.cell width="15%">
+    {{$row->stock_masuk}}
+</x-livewire-tables::bs5.table.cell>
+<x-livewire-tables::bs5.table.cell width="15%">
+    {{$row->stock_keluar}}
+</x-livewire-tables::bs5.table.cell>
+<x-livewire-tables::bs5.table.cell width="15%">
+    {{$row->stock_saldo}}
+</x-livewire-tables::bs5.table.cell>
