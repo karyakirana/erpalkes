2e5bc247cd6a3e522b11e896f0f735548e681af1
toby <tobytriantono@gmail.com>
2023-01-06 13:17:11 +0700

    update master

--- resources/views/components/metronics/sidebar.blade.php
+++ resources/views/components/metronics/sidebar.blade.php
@@ -583,18 +583,6 @@
                             <!--end:Menu link-->
                         </div>
                         <!--end:Menu item-->
-                        <!--begin:Menu item-->
-                        <div class="menu-item">
-                            <!--begin:Menu link-->
-                            <a class="menu-link" href="{{url('master/lokasi')}}">
-													<span class="menu-bullet">
-														<span class="bullet bullet-dot"></span>
-													</span>
-                                <span class="menu-title">Lokasi</span>
-                            </a>
-                            <!--end:Menu link-->
-                        </div>
-                        <!--end:Menu item-->
                     </div>
                     <!--end:Menu sub-->
                 </div>
