e932d458656ec1d9c8ffdd6198dce494d4df3b6d
toby <tobytriantono@gmail.com>
2023-01-06 15:24:47 +0700

    update supplier set table

--- resources/views/livewire-tables/rows/supplier_set_table.blade.php
+++ resources/views/livewire-tables/rows/supplier_set_table.blade.php
@@ -7,9 +7,12 @@
 <x-livewire-tables::table.cell width="20%">
     {{ucwords($row->nama)}}
 </x-livewire-tables::table.cell>
-<x-livewire-tables::table.cell width="25%">
+<x-livewire-tables::table.cell width="20%">
     {{$row->alamat}}
 </x-livewire-tables::table.cell>
+<x-livewire-tables::table.cell width="20%">
+    {{$row->regency->name}}
+</x-livewire-tables::table.cell>
 <x-livewire-tables::table.cell width="15%" class="text-center">
-    <x-button.btn-table wire:click="$emit('setSupplier', {{$row->id}})">Set</x-button.btn-table>
+    <x-button.btn-table wire:click="$emit('setSupplier', {{$row->id}})">SET</x-button.btn-table>
 </x-livewire-tables::table.cell>
